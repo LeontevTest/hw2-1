2e7a2f968d54e5f2b8d50cd46f71733ff4ced62e
Aleks test <7501130l@gmail.com>
2024-01-29 17:25:54 +0300

    gitignor added

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Бонусы за покупку билетa");
+
+        int amount = 75215;
+
+        int mil = 20;
+
+        System.out.println(amount / mil);
+        
+
+
+    }
+}
